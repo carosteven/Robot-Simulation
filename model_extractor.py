a70c85b96772658bff1bc668eb2a1c3cb3dcf6c5
Steven Caro <64928637+carosteven@users.noreply.github.com>
2024-10-09 16:54:46 -0400

    Added stat extraction to model_extractor

--- model_extractor.py
+++ model_extractor.py
@@ -3,8 +3,8 @@ from collections import namedtuple
 
 options = False
 
-checkpoint_path = 'checkpoint/checkpoint-basic-pr-lbu.pt'
-model_path = 'model_weights/model_basic-pr-lbu.pt'
+checkpoint_path = 'checkpoint/checkpoint-basic-states-boxes.pt'
+model_path = 'model_weights/model-basic-states-boxes.pt'
 Transition = namedtuple('Transition',
                         ('state', 'action', 'next_state', 'reward', 'distance'))
 
@@ -14,5 +14,10 @@ if options:
     for i in range(2):
         torch.save(checkpoint[f'policy_state_dict_{i}'], model_path[:-3] + f'_{i}.pt')
 else:
-    torch.save(checkpoint['policy_state_dict_0'], model_path)
+    model = {
+        'policy_state_dict_0': checkpoint['policy_state_dict_0'],
+        'stats': checkpoint['stats'],
+    }
+    # torch.save(checkpoint['policy_state_dict_0'], model_path)
+    torch.save(model, model_path)
 # print(checkpoint['epsilon'])
